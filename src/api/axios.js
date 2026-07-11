05d048b7e2cf8ed73995c5553a656ad252e90950
Vishal Nanaware <nanawarev41@gmail.com>
2026-07-11 22:39:05 +0530

    Connect frontend with production backend

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://onlinefoodorder-backend.onrender.com/api',
 });
 
 api.interceptors.request.use((config) => {
